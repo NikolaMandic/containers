c53b5a7cc84c7d96c26771c68e4f8e64f65935cd
NikolaMandic <mn080202@gmail.com>
2015-07-24 01:49:49 +0200

    WORKS git push origin master! YAY

--- haraka/start/bootstrap.sh
+++ haraka/start/bootstrap.sh
@@ -2,5 +2,5 @@ npm install -g Haraka
 haraka -i /root/haraka
 echo ".*" >> haraka/config/connect.rdns_access.whitelist
 echo ".*" >> haraka/config/mail_from.access.whitelist
-openssl req     -new     -newkey rsa:8192     -days 365     -nodes     -x509     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=`hostname -f`"     -keyout /root/haraka/config/tls_key.pem -out /root/haraka/tls_cert.pem
+openssl req     -new     -newkey rsa:8192     -days 365     -nodes     -x509     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=`hostname -f`"     -keyout /root/haraka/config/tls_key.pem -out /root/haraka/config/tls_cert.pem
 curl -LSso /root/haraka/config/plugins  https://gist.githubusercontent.com/NikolaMandic/604a51c52c2531bd459a/raw/314e878e6670722cd004cf4b3428115b654dadfe/plugins 

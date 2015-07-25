0d4a5844909d0dd37bd2819634ece7c15bb49174
NikolaMandic <mn080202@gmail.com>
2015-07-25 06:07:07 +0200

    kinda ok

--- dovecot/start/bootstrap.sh
+++ dovecot/start/bootstrap.sh
@@ -7,6 +7,26 @@ imap_client_workarounds = tb-extra-mailbox-sep
 }"
 >> /etc/dovecot/dovecot.conf
 sed -i 's/#!include auth-passwdfile.conf.ext/!include auth-passwdfile.conf.ext/' /etc/dovecot/conf.d/10-auth.conf
+echo "user:{plain}pass:1000:1000::/home/user" >> /etc/dovecot/users
+echo "
+passdb {
+  driver = passwd-file
+  args = scheme=plain-md5 username_format=%n /etc/dovecot/users
+}
+
+userdb {
+  driver = passwd-file
+  args = username_format=%n /etc/dovecot/users
+
+  # Default fields that can be overridden by passwd-file
+  #default_fields = quota_rule=*:storage=1G
+
+  # Override fields from passwd-file
+  #override_fields = home=/home/virtual/%u
+}
+">>/etc/dovecot/conf.d/auth-passwdfile.conf.ext
+mkdir /home/user/mail
+mkdir /var/mail/user
 useradd $USERR
 mkdir /home/$USERR
 chown -R $USER: /home/$USERR

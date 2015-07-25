47e24419d39882e186419466237381acc64f61ee
NikolaMandic <mn080202@gmail.com>
2015-07-25 15:50:52 +0200

    stuff

--- qmail/start/bootstrap.sh
+++ qmail/start/bootstrap.sh
@@ -1,5 +1,7 @@
 openssl req     -new     -newkey rsa:8192     -days 365     -nodes     -x509     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=`hostname -f`"     -keyout /root/haraka/config/tls_key.pem -out /root/haraka/config/tls_cert.pem
 sed -i '1s/.*/gcc -O2 -include \/usr\/include\/errno.h/g' /root/qmail/conf-cc
+sed -i '1s/.*/gcc -O2 -include \/usr\/include\/errno.h/g' /root/ucspi/conf-cc
+sed -i '1s/.*/gcc -O2 -include \/usr\/include\/errno.h/g' /root/daemontools/src/conf-cc
 useradd alias
 useradd qmaild
 useradd qmaill

ae00ddce837723acaa3e03f0788f70daf47ec527
NikolaMandic <mn080202@gmail.com>
2015-07-25 05:33:28 +0200

    added config change

--- dovecot/start/bootstrap.sh
+++ dovecot/start/bootstrap.sh
@@ -6,4 +6,8 @@ login_greeting_capability = yes
 imap_client_workarounds = tb-extra-mailbox-sep
 }"
 >> /etc/dovecot/dovecot.conf
+sed -i 's/#!include auth-passwdfile.conf.ext/!include auth-passwdfile.conf.ext/' /etc/dovecot/conf.d/10-auth.conf
+useradd $USERR
+mkdir /home/$USERR
+chown -R $USER: /home/$USERR
 openssl req     -new     -newkey rsa:8192     -days 365     -nodes     -x509     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=`hostname -f`"     -keyout /etc/dovecot/private/dovecot.pem -out /etc/dovecot/dovecot.pem

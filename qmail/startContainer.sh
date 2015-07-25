f815a419fc0ad7ccfdb1fc844b0fff0acb5b9d38
NikolaMandic <mn080202@gmail.com>
2015-07-25 18:26:34 +0200

    start

--- qmail/startContainer.sh
+++ qmail/startContainer.sh
@@ -1,3 +1,1 @@
-docker run -v `pwd`/daemontools:/root/da
-emontools -v `pwd`/ucspi:/root/ucspi -v `pwd`/qmail:/root/qmail -v `pwd`/start:/root/start -p 1
-10:110 -p 995:995  --hostname="test.nikola.link" -e "USERR=user" -it qmail bash
+docker run -v `pwd`/config:/root/config -v `pwd`/admin:/root/admin -v `pwd`/ucspi:/root/ucspi -v `pwd`/qmail:/root/qmail -v `pwd`/start:/root/start -p 110:110 -p 995:995  --hostname="test.nikola.link" -e "USERR=user" -it qmail bash

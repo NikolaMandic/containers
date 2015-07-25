379eb987fb40a2a6079d40a590e479c74d5d15f6
NikolaMandic <mn080202@gmail.com>
2015-07-25 05:35:01 +0200

    added config change

--- dovecot/startContainer.sh
+++ dovecot/startContainer.sh
@@ -1,1 +1,1 @@
-docker run -v `pwd`/start:/root/start -p 110:110  --hostname="$1" -it dovecot bash
+docker run -v `pwd`/start:/root/start -p 110:110 -p 995:995  --hostname="test.nikola.link" -e "USERR=user" -it dovecot bash
